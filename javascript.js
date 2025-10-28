f8771ef593dd5200cc72a1f6c5218ad3a367a566
Ellin <1833921+Ellin@users.noreply.github.com>
2025-10-28 19:00:07 +0000

    Write pseudocode for game logic

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// Function: Get computer choice
+// Generate random number from 0-2
+// Assign computerChoice: 0 = "rock", 1 = "paper", 2 = "scissors"
+// Return computerChoice
+
+// Function: Get human choice
+// Prompt for input
+// Standardize input to lower case ("Rock"-> "rock")
+// Return humanChoice
+
+
+// Create function playRound (humanChoice, computerChoice): Play single round
+// Compare humanChoice vs cpuHand
+// if humanChoice  === cpuHand
+// display message declaring a draw
+
+// else if humanChoice === "rock" && computerChoice === "scissors"
+//  OR humanChoice === "paper" && computerChoice === "rock",
+//  OR humanChoice === "scissors" && computerChoice === "paper"
+// then display message declaring that the human wins
+// increment humanScore
+
+// else display message declaring the computer wins
+// increment computerScore
+
+
+// initialize variable humanScore = 0
+// initialize variable computerScore = 0;
+
+// Create function playGame
+// calls playRound 5 times
+// Keeps track of scores and declares a winner at the end
